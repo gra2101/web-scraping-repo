4402d232e84cc926f98f905d69b106e12f7c1aea
Grace O <75256483+gra2101@users.noreply.github.com>
2022-07-12 15:56:17 +0100

    update'

--- python-web-scraping.py
+++ python-web-scraping.py
@@ -1,2 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+
+url="http://books.toscrape.com/"
+data=requests.get(url)
+
+#print(r.content)
+
+#to get data
+soup=BeautifulSoup(data.content)
+
+#to print data
+print(soup)
+
+#to convert data into a structured one
+print(soup.prettify())
+
+#to find all anchor tags
+print(soup.find_all("a"))
+
