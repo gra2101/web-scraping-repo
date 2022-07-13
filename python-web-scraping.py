028bc042d9a111ecdce95a3e2d2b5c39fa340900
Grace O <75256483+gra2101@users.noreply.github.com>
2022-07-13 23:12:41 +0100

    'update'

--- python-web-scraping.py
+++ python-web-scraping.py
@@ -1,20 +1,34 @@
+from pyclbr import Class
 import requests
 from bs4 import BeautifulSoup
 
 url="http://books.toscrape.com/"
 data=requests.get(url)
 
-#print(r.content)
+soup=BeautifulSoup(data.content, "html.parser")
+soup.prettify()
+soup.find_all('div')
+listdata= soup.find('ul', {'class':'nav nav-list'})
+# for li in listdata:
+#     print(li.get_text())
+print(listdata.find_all())
+
+
 
 #to get data
-soup=BeautifulSoup(data.content)
+# soup=BeautifulSoup(data.content)
 
-#to print data
-print(soup)
+# #to print data
+# print(soup)
 
-#to convert data into a structured one
-print(soup.prettify())
+# #to convert data into a structured one
+# print(soup.prettify())
 
-#to find all anchor tags
-print(soup.find_all("a"))
+# #to find all anchor tags
+# print(soup.find_all("a"))
+# # for link in soup.find_all("a"):
+# #     print(link.text)
 
+# soup.find_all("li")
+# for text in soup.find_all("li"):
+#     print(text)
